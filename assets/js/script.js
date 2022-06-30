3f73c211d05503364b5cd9ea4b57cffd480495f6
lshillman <lshillman@users.noreply.github.com>
2022-06-29 22:39:29 -0700

    create timeblocks and declare associated vars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
 console.log("I'm a JavaScript file linked to this page!");
+
+var text9am = $('#9amText');
+var btn9am = $('#9amBtn');
+var text10am = $('#10amText');
+var btn10am = $('#10amBtn');
+var text11am = $('#11amText');
+var btn11am = $('#11amBtn');
+var text12pm = $('#12pmText');
+var btn12pm = $('#12pmBtn');
+var text1pm = $('#1pmText');
+var btn1pm = $('#1pmBtn');
+var text2pm = $('#2pmText');
+var btn2pm = $('#2pmBtn');
+var text3pm = $('#3pmText');
+var btn3pm = $('#3pmBtn');
+var text4pm = $('#4pmText');
+var btn4pm = $('#4pmBtn');
+var text5pm = $('#5pmText');
+var btn5pm = $('#5pmBtn');
+
+var now = moment();
+
